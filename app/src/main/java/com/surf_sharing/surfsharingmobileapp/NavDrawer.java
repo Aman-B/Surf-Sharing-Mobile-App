d41708f34b5b61c94bac1fe935603fcec8dd7554
Aran Nolan <nolanar@tcd.ie>
2017-03-04 21:22:19 +0000

    Remove overflow button from navigation drawer activity

--- app/src/main/java/com/surf_sharing/surfsharingmobileapp/NavDrawer.java
+++ app/src/main/java/com/surf_sharing/surfsharingmobileapp/NavDrawer.java
@@ -43,12 +43,12 @@ public class NavDrawer extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_drawer, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
