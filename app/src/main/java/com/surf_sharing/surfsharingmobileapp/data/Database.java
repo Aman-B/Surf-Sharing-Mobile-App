852efd395f8b8c5b682e6e65dff0e97a327c7a14
Aran Nolan <nolanar@tcd.ie>
2017-03-07 17:10:46 +0000

    Add Database class for interacting with Firebase database

---
+++ app/src/main/java/com/surf_sharing/surfsharingmobileapp/data/Database.java
@@ -0,0 +1,10 @@
+package com.surf_sharing.surfsharingmobileapp.data;
+
+/**
+ * Created by aran on 07/03/17.
+ *
+ * Contains functions for interacting with the Firebase realtime databases
+ */
+public class Database {
+
+}
